ba1e5a0c8a39fda6a3f9c81ea6c12681ce5e24e5
Vitor Gabriel De Oliveira <vitorgabrieldeoliveiragit@gmail.com>
2023-07-05 10:13:22 -0300

    Build system generate list, update count users

--- login.js
+++ login.js
@@ -4,11 +4,7 @@ function Verificar(){
     let user = document.getElementById('Email').value;
     let senha = document.getElementById('Senha').value;
 
-    if(!user || !senha){
-        alert("Campos de preenchimento obrigatorio. Favor preencher!");
-    } else{
-        window.location.href = "aula2.html";
-    };
+    if(!user || !senha)  alert("Campos de preenchimento obrigatorio. Favor preencher!"); else  window.location.href = "aula2.html";
 };
 
 var dadosLista = [];
@@ -17,6 +13,26 @@ const salvaUser = () => {
     let nomeUser =  $('#nomeUser').val();
     if (nomeUser) {
         dadosLista.push(nomeUser);
-        console.log(dadosLista);
+        countUsers();
+        criaLista();
     };
 };
+
+const criaLista = () => {
+    let tabela = document.querySelector('#tabela').innerHTML = `<tr style='background: none'>
+                                                                      <th>Nome Usuário</th>
+                                                                      <th>Acões</th>
+                                                                  </tr>`;
+    for(let i = 0; i <= (dadosLista.length - 1); i ++) {
+        tabela += `<tr>
+                        <td>${dadosLista[i]}</td>
+                        <td>
+                            <button class="btn btn-success" onclick="">Editar</button>
+                            <button class="btn btn-danger" onclick="">Deletar</button>
+                        </td>
+                   </tr>`;
+                   document.querySelector('#tabela').innerHTML = tabela;
+    };
+};
+
+const countUsers = () => document.querySelector('#countUsers').innerHTML = `Usuários: ${dadosLista.length}`;
